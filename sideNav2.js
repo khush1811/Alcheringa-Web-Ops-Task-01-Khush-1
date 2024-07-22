eea9a5bc0e0d87078903803a7d53ab7da59daa7c
khush1811 <khushkothawala35@gmail.com>
2024-07-22 20:58:31 +0530

    Add files via upload

---
+++ sideNav2.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.sub-btn').click(function(){
+        $(this).next('.sub-menu').slideToggle();
+        $(this).find('.dropdown').toggleClass('rotate');
+    });
+
+    $('.menu-btn').click(function(){
+        $('.side-bar').addClass('active');
+    });
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
